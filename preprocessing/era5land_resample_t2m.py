eb37cb30169bd9b7bed1dfb7e61e7b625a0199c5
Pascal Horton <pascal.horton@unibe.ch>
2024-03-07 10:30:50 +0100

    Making the preprocessing scripts more readable.

--- preprocessing/era5land_resample_t2m.py
+++ preprocessing/era5land_resample_t2m.py
@@ -13,24 +13,30 @@ Y_END = 2023
 
 for year in range(Y_START, Y_END + 1):
     print(f'Processing year {year}')
+
     # Read the original data
     data_path = Path(DATA_DIR) / f't2m-{year}.grib'
     ds = xr.open_dataset(data_path, engine='cfgrib')
+
     # Resample to daily data
     t2m = ds['t2m']
     step = ds['step']
+
     # Aggregate over the step dimension
     t2m_min = t2m.min(dim='step')
     t2m_max = t2m.max(dim='step')
     t2m_mean = t2m.mean(dim='step')
+
     # Rename the variables
     t2m_min = t2m_min.rename('t2m_min')
     t2m_max = t2m_max.rename('t2m_max')
+
     # Drop the first day of the year as it is incomplete
     t = ds['time']
     t2m_min = t2m_min.sel(time=t.values[1:])
     t2m_max = t2m_max.sel(time=t.values[1:])
     t2m_mean = t2m_mean.sel(time=t.values[1:])
+
     # Save to NetCDF
     output_path_min = Path(OUTPUT_DIR) / f't2m_min-{year}.nc'
     output_path_max = Path(OUTPUT_DIR) / f't2m_max-{year}.nc'
@@ -38,6 +44,7 @@ for year in range(Y_START, Y_END + 1):
     t2m_min.to_netcdf(output_path_min, format='NETCDF4_CLASSIC')
     t2m_max.to_netcdf(output_path_max, format='NETCDF4_CLASSIC')
     t2m_mean.to_netcdf(output_path_mean, format='NETCDF4_CLASSIC')
+
     # Close the files
     ds.close()
     t2m_min.close()
--- preprocessing/era5land_resample_tp.py
+++ preprocessing/era5land_resample_tp.py
@@ -13,21 +13,27 @@ Y_END = 2023
 
 for year in range(Y_START, Y_END + 1):
     print(f'Processing year {year}')
+
     # Read the original data
     data_path = Path(DATA_DIR) / f'prec-{year}.grib'
     ds = xr.open_dataset(data_path, engine='cfgrib')
+
     # Resample to daily data
     precipitation = ds['tp']
     step = ds['step']
     precipitation = precipitation.sel(step=step.values[-1])
+    
     # Remove the step dimension
     precipitation = precipitation.drop_vars('step')
+
     # Drop the last day of the year as it is incomplete
     t = ds['time']
     precipitation = precipitation.sel(time=t.values[0:-1])
+
     # Save to NetCDF
     output_path = Path(OUTPUT_DIR) / f'tp-{year}.nc'
     precipitation.to_netcdf(output_path, format='NETCDF4_CLASSIC')
+
     # Close the files
     ds.close()
     precipitation.close()
