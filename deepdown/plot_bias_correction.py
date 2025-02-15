90fdbeac7483d4f9811ac876698f9b1ef1374085
Pascal Horton <pascal.horton@unibe.ch>
2025-02-15 12:22:42 +0100

    Adding plotting scripts.

---
+++ deepdown/plot_bias_correction.py
@@ -0,0 +1,161 @@
+import argparse
+import logging
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import xarray as xr
+from deepdown.config import Config
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def plot_bias_correction(conf):
+    data_path = Path(conf.path_output)
+
+    # Load input_data_hist_original.nc
+    target_data = xr.open_dataset(data_path / "target_data_hist_original.nc")
+    target_data = target_data.load()
+    input_data_hist = xr.open_dataset(data_path / "input_data_hist_original.nc")
+    input_data_hist = input_data_hist.load()
+    output_data_hist = xr.open_dataset(data_path / "input_data_hist_debiased.nc")
+    output_data_hist = output_data_hist.load()
+    input_data_clim = xr.open_dataset(data_path / "input_data_clim_original.nc")
+    input_data_clim = input_data_clim.load()
+    output_data_clim = xr.open_dataset(data_path / "input_data_clim_debiased.nc")
+    output_data_clim = output_data_clim.load()
+
+    # Plot the maps
+    for var in conf.target_vars:
+        target_data_mean = target_data[var].mean(dim='time')
+        input_data_hist_mean = input_data_hist[var].mean(dim='time')
+        output_data_hist_mean = output_data_hist[var].mean(dim='time')
+        input_data_clim_mean = input_data_clim[var].mean(dim='time')
+        output_data_clim_mean = output_data_clim[var].mean(dim='time')
+
+        min_val = min(input_data_hist_mean.min().values,
+                      output_data_hist_mean.min().values,
+                      input_data_clim_mean.min().values,
+                      output_data_clim_mean.min().values)
+        max_val = max(input_data_hist_mean.max().values,
+                      output_data_hist_mean.max().values,
+                      input_data_clim_mean.max().values,
+                      output_data_clim_mean.max().values)
+
+        if var == 'tp':
+            cmap = 'Blues'
+        elif var == 't':
+            cmap = 'turbo'
+        else:
+            cmap = 'viridis'
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7),
+                               subplot_kw={'projection': ccrs.PlateCarree()})
+        target_data_mean.plot(ax=ax, transform=ccrs.PlateCarree(), cmap=cmap,
+                              cbar_kwargs={'shrink': 0.5}, vmin=min_val,
+                              vmax=max_val)
+        ax.add_feature(cfeature.COASTLINE)
+        ax.add_feature(cfeature.BORDERS)
+        ax.set_title(f"Target {var}")
+        ax.axis('off')
+        plt.tight_layout()
+        plt.savefig(f"{var}_target.png")
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7),
+                               subplot_kw={'projection': ccrs.PlateCarree()})
+        input_data_hist_mean.plot(ax=ax, transform=ccrs.PlateCarree(), cmap=cmap,
+                                  cbar_kwargs={'shrink': 0.5}, vmin=min_val,
+                                  vmax=max_val)
+        ax.add_feature(cfeature.COASTLINE)
+        ax.add_feature(cfeature.BORDERS)
+        ax.set_title(f"Input {var} (historical)")
+        ax.axis('off')
+        plt.tight_layout()
+        plt.savefig(f"{var}_input_hist_original.png")
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7),
+                               subplot_kw={'projection': ccrs.PlateCarree()})
+        output_data_hist_mean.plot(ax=ax, transform=ccrs.PlateCarree(), cmap=cmap,
+                                   cbar_kwargs={'shrink': 0.5}, vmin=min_val,
+                                   vmax=max_val)
+        ax.add_feature(cfeature.COASTLINE)
+        ax.add_feature(cfeature.BORDERS)
+        ax.set_title(f"Debiased {var} (historical)")
+        ax.axis('off')
+        plt.tight_layout()
+        plt.savefig(f"{var}_input_hist_debiased.png")
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7),
+                               subplot_kw={'projection': ccrs.PlateCarree()})
+        input_data_clim_mean.plot(ax=ax, transform=ccrs.PlateCarree(), cmap=cmap,
+                                  cbar_kwargs={'shrink': 0.5}, vmin=min_val,
+                                  vmax=max_val)
+        ax.add_feature(cfeature.COASTLINE)
+        ax.add_feature(cfeature.BORDERS)
+        ax.set_title(f"Input {var} (future)")
+        ax.axis('off')
+        plt.tight_layout()
+        plt.savefig(f"{var}_input_clim_original.png")
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7),
+                               subplot_kw={'projection': ccrs.PlateCarree()})
+        output_data_clim_mean.plot(ax=ax, transform=ccrs.PlateCarree(), cmap=cmap,
+                                   cbar_kwargs={'shrink': 0.5}, vmin=min_val,
+                                   vmax=max_val)
+        ax.add_feature(cfeature.COASTLINE)
+        ax.add_feature(cfeature.BORDERS)
+        ax.set_title(f"Debiased {var} (future)")
+        ax.axis('off')
+        plt.tight_layout()
+        plt.savefig(f"{var}_input_clim_debiased.png")
+
+    exit()
+
+    # For each variable, do a scatter plot of the original (input_data_clim) vs. debiased data (output_data_clim)
+    for var in conf.target_vars:
+        fig, ax = plt.subplots()
+        ax.scatter(input_data_clim[var].values.flatten(), output_data_clim[var].values.flatten(), s=1)
+        ax.set_xlabel(f"Original {var}")
+        ax.set_ylabel(f"Debiased {var}")
+        ax.set_title(f"Original vs. Debiased {var}")
+        plt.tight_layout()
+        plt.show()
+
+    target_data_hist = xr.open_dataset(data_path / "target_data_hist_original.nc")
+    target_data_hist = target_data_hist.load()
+
+    # For each variable, do a scatter plot of the original vs. target data
+    for var in conf.target_vars:
+        fig, ax = plt.subplots()
+        ax.scatter(input_data_hist[var].values.flatten(), target_data_hist[var].values.flatten(), s=1)
+        ax.set_xlabel(f"Original {var}")
+        ax.set_ylabel(f"Target {var}")
+        ax.set_title(f"Original vs. Target {var}")
+        plt.tight_layout()
+        plt.show()
+
+    # For each variable, do a scatter plot of the debiased vs. target data
+    for var in conf.target_vars:
+        fig, ax = plt.subplots()
+        ax.scatter(output_data_hist[var].values.flatten(), target_data_hist[var].values.flatten(), s=1)
+        ax.set_xlabel(f"Debiased {var}")
+        ax.set_ylabel(f"Target {var}")
+        ax.set_title(f"Debiased vs. Target {var}")
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("--config_file", default='../config.yaml',
+                           help="Path to the .yml config file")
+    args = argParser.parse_args()
+
+    logger.info("Loading configuration...")
+    conf = Config(args)
+    conf.print()
+
+    logger.info("Starting bias correction")
+    plot_bias_correction(conf.get())
---
+++ deepdown/plot_coarsened_mch_data.py
@@ -0,0 +1,76 @@
+import argparse
+import logging
+import numpy as np
+import xarray as xr
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+from deepdown.utils.debiaser_utils import prepare_for_sbck, debias_with_sbck
+from deepdown.utils.data_loader import DataLoader
+from deepdown.config import Config
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def plot_maps(conf):
+    # Load target data for the historical period
+    target_data_hist = DataLoader(path_tmp=conf.path_tmp)
+    target_data_hist.load(conf.period_hist_start, conf.period_hist_end,
+                          conf.path_targets)
+
+    # Load input data (e.g. climate model) for the historical period
+    input_data_hist = DataLoader(path_tmp=conf.path_tmp)
+    input_data_hist.load(conf.period_hist_start, conf.period_hist_end, conf.path_inputs)
+
+    # Load input data (e.g. climate model) for the future period
+    input_data_clim = DataLoader(path_tmp=conf.path_tmp)
+    input_data_clim.load(conf.period_clim_start, conf.period_clim_end, conf.path_inputs)
+
+    # Coarsen the target data to the resolution of the input data
+    target_data_hist_coarsened = DataLoader(path_tmp=conf.path_tmp)
+    target_data_hist_coarsened.load(conf.period_hist_start, conf.period_hist_end,
+                                    conf.path_targets)
+    target_data_hist_coarsened.coarsen(
+        x_axis=input_data_hist.data.x, y_axis=input_data_hist.data.y,
+        from_proj='CH1903_LV95', to_proj='WGS84')
+
+    # Plot the maps
+    for var in conf.target_vars:
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+        mean_val = target_data_hist.data[var].mean(dim='time')
+        ax.imshow(mean_val, cmap='viridis')
+        # ax[0].set_title(f"Input {var}")
+        ax.axis('off')
+        plt.tight_layout()
+        plt.show()
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+        mean_val = input_data_hist.data[var].mean(dim='time')
+        ax.imshow(mean_val, cmap='viridis')
+        # ax[0].set_title(f"Input {var}")
+        ax.axis('off')
+        plt.tight_layout()
+        plt.show()
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+        mean_val = target_data_hist_coarsened.data[var].mean(dim='time')
+        ax.imshow(mean_val, cmap='viridis')
+        # ax[0].set_title(f"Input {var}")
+        ax.axis('off')
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("--config_file", default='../config.yaml',
+                           help="Path to the .yml config file")
+    args = argParser.parse_args()
+
+    logger.info("Loading configuration...")
+    conf = Config(args)
+    conf.print()
+
+    logger.info("Starting bias correction")
+    plot_maps(conf.get())
