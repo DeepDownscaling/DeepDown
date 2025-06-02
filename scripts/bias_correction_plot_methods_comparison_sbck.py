26a00461e358f3010f44e3ad7feb979105054cee
Pascal Horton <pascal.horton@unibe.ch>
2025-06-02 18:51:47 +0200

    Computing additional statistics.

--- scripts/bias_correction_plot_methods_comparison_sbck.py
+++ scripts/bias_correction_plot_methods_comparison_sbck.py
@@ -14,23 +14,17 @@ from deepdown.utils.utils import spearman_correlation
 warnings.filterwarnings("ignore", category=RuntimeWarning,
                         message="Mean of empty slice")
 warnings.filterwarnings("ignore", category=RuntimeWarning,
+                        message="All-NaN slice encountered")
+warnings.filterwarnings("ignore", category=RuntimeWarning,
                         message="Degrees of freedom <= 0 for slice")
 
 PLOTS = ['maps_for_bc_method', 'maps_for_rcm', 'maps_correl_for_rcm'] # List of plots
-PLOTS = ['maps_for_bc_method']
 CORREL_VARS = ['tp', 't']  # Variables to compute correlation for
 CORREL_MIN = -0.5  # Minimum value for correlation plots
 CORREL_MAX = 0.5  # Maximum value for correlation plots
-TITLES = {
-    'target_data_hist_original.nc': 'Target data (control period)',
-    'input_data_hist_original.nc': 'RCM (control period, original)',
-    'input_data_hist_debiased.nc': 'RCM (control period, debiased)',
-    'input_data_proj_original.nc': 'RCM (future, original)',
-    'input_data_proj_debiased.nc': 'RCM (future, debiased)'
-}
 
 
-def plot_maps_mean_for_bc_method(conf, files, method, var, epsg=2056):
+def plot_maps_stat_for_bc_method(conf, files, titles, method, var, stat, epsg=2056):
     models = conf.RCMs
     path_output = Path(conf.path_output)
     crs = ccrs.epsg(epsg)
@@ -51,31 +45,31 @@ def plot_maps_mean_for_bc_method(conf, files, method, var, epsg=2056):
                 ds = xr.open_dataset(file_path).load()
 
                 if var in ds:
-                    data = ds[var].mean(dim="time")
+                    data = _compute_stat(ds, stat, var)
+                    if data is None:
+                        continue
 
-                    if v_min is None and col == 0:
-                        v_min = data.min() * 0.8
-                        v_max = data.max() * 1.2
+                    v_min, v_max = _get_min_max(data, v_min, v_max, var, stat)
 
                     # Plot on corresponding subplot
                     ax = fig.add_subplot(gs[row, col], projection=crs)
                     data.plot(ax=ax, transform=ccrs.PlateCarree(), cmap=_get_cmap(var),
                               add_colorbar=False, vmin=v_min, vmax=v_max)
-                    ax.set_title(f"{model}\n{TITLES[data_type]}", fontsize=12)
+                    ax.set_title(f"{model}\n{titles[data_type]}", fontsize=12)
                     ax.add_feature(cfeature.BORDERS)
 
     plt.tight_layout()
 
     # Save the figure
-    plt.savefig(path_output / f"maps_mean_for_bc_method_{method}_{var}.png",
+    plt.savefig(path_output / f"maps_{stat}_for_bc_method_{method}_{var}.png",
                 dpi=300, bbox_inches="tight")
-    plt.savefig(path_output / f"maps_mean_for_bc_method_{method}_{var}.pdf",
+    plt.savefig(path_output / f"maps_{stat}_for_bc_method_{method}_{var}.pdf",
                 dpi=300, bbox_inches="tight")
     plt.close(fig)
     print(f"Saved in {path_output}")
 
 
-def plot_maps_mean_for_rcm(conf, files, model, var, epsg=2056):
+def plot_maps_stat_for_rcm(conf, files, titles, model, var, stat, epsg=2056):
     methods = conf.bc_methods
     path_output = Path(conf.path_output)
     crs = ccrs.epsg(epsg)
@@ -96,25 +90,25 @@ def plot_maps_mean_for_rcm(conf, files, model, var, epsg=2056):
                 ds = xr.open_dataset(file_path).load()
 
                 if var in ds:
-                    data = ds[var].mean(dim="time")
+                    data = _compute_stat(ds, stat, var)
+                    if data is None:
+                        continue
 
-                    if v_min is None and col == 0:
-                        v_min = data.min() * 0.8
-                        v_max = data.max() * 1.2
+                    v_min, v_max = _get_min_max(data, v_min, v_max, var, stat)
 
                     # Plot on corresponding subplot
                     ax = fig.add_subplot(gs[row, col], projection=crs)
                     data.plot(ax=ax, transform=ccrs.PlateCarree(), cmap=_get_cmap(var),
                               add_colorbar=False, vmin=v_min, vmax=v_max)
-                    ax.set_title(f"{method}\n{TITLES[data_type]}", fontsize=12)
+                    ax.set_title(f"{method}\n{titles[data_type]}", fontsize=12)
                     ax.add_feature(cfeature.BORDERS)
 
     plt.tight_layout()
 
     # Save the figure
-    plt.savefig(path_output / f"maps_mean_for_rcm_{model}_{var}.png", dpi=300,
+    plt.savefig(path_output / f"maps_{stat}_for_rcm_{model}_{var}.png", dpi=300,
                 bbox_inches="tight")
-    plt.savefig(path_output / f"maps_mean_for_rcm_{model}_{var}.pdf", dpi=300,
+    plt.savefig(path_output / f"maps_{stat}_for_rcm_{model}_{var}.pdf", dpi=300,
                 bbox_inches="tight")
     plt.close(fig)
     print(f"Saved in {path_output}")
@@ -131,18 +125,18 @@ def plot_maps_correl_for_rcm(conf, files, model, plot_diff=False, epsg=2056):
     assert len(vars) == 2
 
     if plot_diff:
-        t_prefix = "Difference in Spearman correlation"
+        t_prefix = "Diff. rank correl."
     else:
-        t_prefix = "Spearman correlation"
+        t_prefix = "Rank correlation"
 
     # Loop through the seasons to make 1 plot for each
     for season in ['DJF', 'MAM', 'JJA', 'SON']:
-        fig = plt.figure(figsize=(10, len(methods) * 3.3))
+        fig = plt.figure(figsize=(7, len(methods) * 3.3))
         gs = gridspec.GridSpec(len(methods) + 2, 2, figure=fig)
 
         # Load the reference data
-        ds = xr.open_dataset(path_output / methods[0] / model / files[0])
-        ds = ds.where(ds.time.dt.season == season, drop=True).load()
+        ds = xr.open_dataset(path_output / methods[0] / model / files[0]).load()
+        ds = ds.where(ds.time.dt.season == season, drop=True)
 
         # Compute the spearman's rank correlation
         correl_ref = spearman_correlation(ds[vars[0]], ds[vars[1]], dim="time")
@@ -153,8 +147,8 @@ def plot_maps_correl_for_rcm(conf, files, model, plot_diff=False, epsg=2056):
         ax.add_feature(cfeature.BORDERS)
 
         # Load the RCM data for the control period
-        ds = xr.open_dataset(path_output / methods[0] / model / files[1])
-        ds = ds.where(ds.time.dt.season == season, drop=True).load()
+        ds = xr.open_dataset(path_output / methods[0] / model / files[1]).load()
+        ds = ds.where(ds.time.dt.season == season, drop=True)
 
         # Compute the spearman's rank correlation
         correl_model_hist = spearman_correlation(ds[vars[0]], ds[vars[1]], dim="time")
@@ -169,8 +163,8 @@ def plot_maps_correl_for_rcm(conf, files, model, plot_diff=False, epsg=2056):
         ax.add_feature(cfeature.BORDERS)
 
         # Load the RCM data for the future period
-        ds = xr.open_dataset(path_output / methods[0] / model / files[3])
-        ds = ds.where(ds.time.dt.season == season, drop=True).load()
+        ds = xr.open_dataset(path_output / methods[0] / model / files[3]).load()
+        ds = ds.where(ds.time.dt.season == season, drop=True)
 
         # Compute the spearman's rank correlation
         correl_model_proj = spearman_correlation(ds[vars[0]], ds[vars[1]], dim="time")
@@ -194,9 +188,8 @@ def plot_maps_correl_for_rcm(conf, files, model, plot_diff=False, epsg=2056):
                 file_path = data_path / data_type
 
                 if file_path.exists():
-                    print(f"Loading: {file_path}")
-                    ds = xr.open_dataset(file_path)
-                    ds = ds.where(ds.time.dt.season == season, drop=True).load()
+                    ds = xr.open_dataset(file_path).load()
+                    ds = ds.where(ds.time.dt.season == season, drop=True)
 
                     # Compute the spearman's rank correlation
                     correl = spearman_correlation(ds[vars[0]], ds[vars[1]], dim="time")
@@ -237,6 +230,36 @@ def _get_cmap(var):
         return 'viridis'
 
 
+def _compute_stat(ds, stat, var):
+    if stat == 'mean':
+        data = ds[var].mean(dim="time")
+    elif stat == 'q99':
+        data = ds[var].quantile(0.99, dim="time")
+    elif stat == 'f0':
+        if var != 'tp':
+            return None
+        data = (ds[var].where(ds[var] > 0.1).count(dim="time") /
+                ds[var].count(dim="time"))
+    else:
+        raise ValueError(f"Unknown statistic: {stat}")
+
+    return data
+
+
+def _get_min_max(data, v_min, v_max, var, stat):
+    if v_min is None:
+        if var == 'tp':
+            v_min = 0
+            v_max = data.max() * 1.1
+            if stat == 'f0':
+                v_max = 1.0
+        else:
+            v_min = data.min() * 0.8
+            v_max = data.max() * 1.2
+
+    return v_min, v_max
+
+
 def main():
     # Load the configuration
     conf = Config().get()
@@ -250,16 +273,26 @@ def main():
         "input_data_proj_original.nc",
         "input_data_proj_debiased.nc"
     ]
+    titles = {
+        'target_data_hist_original.nc': 'Target data (control period)',
+        'input_data_hist_original.nc': 'RCM (control period, original)',
+        'input_data_hist_debiased.nc': 'RCM (control period, debiased)',
+        'input_data_proj_original.nc': 'RCM (future, original)',
+        'input_data_proj_debiased.nc': 'RCM (future, debiased)'
+    }
+    stats = ['mean', 'q99', 'f0']  # Statistics to compute
 
     if 'maps_for_bc_method' in PLOTS:
         for method in methods:
             for var in conf.input_vars:
-                plot_maps_mean_for_bc_method(conf, files, method, var)
+                for stat in stats:
+                    plot_maps_stat_for_bc_method(conf, files, titles, method, var, stat)
 
     if 'maps_for_rcm' in PLOTS:
         for model in models:
             for var in conf.input_vars:
-                plot_maps_mean_for_rcm(conf, files, model, var)
+                for stat in stats:
+                    plot_maps_stat_for_rcm(conf, files, titles, model, var, stat)
 
     if 'maps_correl_for_rcm' in PLOTS:
         for model in models:
